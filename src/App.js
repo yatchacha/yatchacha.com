d3e0dfceafe77c9839520db2ad01c8f90a2b0f9d
Hyeon Woo Jo <hyeonwoo5342@gmail.com>
2021-11-16 21:56:34 +0900

    mbti 수정 배포

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './Components/Home';
 import User from './Components/User';
 import MbtiPage from './Components/MbtiPage';
@@ -8,9 +8,19 @@ import { Route, Switch, useHistory, useLocation, MemoryRouter as Router, } from 
 import { AppOutline, MessageOutline, CalendarOutline, AppstoreOutline } from 'antd-mobile-icons';
 import './Style/demo2.css';
 import Bottom from './Components/Template/Bottom';
-
+import Main from './Pages/Main';
+import CalendarPage from './Pages/CalendarPage';
+import { Empty } from 'antd-mobile'
 
 function App() {
+  // 모바일에서 100vh 문제 해결
+  let vh = 0;
+
+  useEffect(() => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
+
   const [title, setTitle] = useState('달차네 마을');
   const [viewBefore, setViewBefore] = useState(null);
 
@@ -24,7 +34,7 @@ function App() {
           <Home />
         </Route>
         <Route exact path='/calendar'>
-          <Todo />
+          <CalendarPage />
         </Route>
         <Route exact path={['/', '/home']}>
           <Main />
@@ -41,30 +51,19 @@ function App() {
       <div className="bottom">
         <Bottom />
       </div>
-      {/* <Route path="/" exact={true} component={Home} />
-      <Route path="/user/:name" exact={true} component={User} />
-      <Route path="/test" exact={true} component={Test} />
-      <Route path="/mbti/:mbti" exact={true} component={MbtiPage} /> */}
     </div>
   );
 }
 
 export default App;
 
-function Todo() {
-  return <div>현재 준비 중입니다</div>;
-}
 function Message() {
-  return <div>현재 준비 중입니다</div>;
+  return (
+    <>
+      <Empty description='현재 준비 중입니다' />
+    </>
+  );
 }
 function PersonalCenter() {
   return <div>현재 준비 중입니다</div>;
-}
-
-function Main() {
-  return (
-    <>
-      <iframe src="https://player.twitch.tv/?channel=yatchacha&parent=yatchacha.com&parent=localhost" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>    
-    </>
-  )
 }
